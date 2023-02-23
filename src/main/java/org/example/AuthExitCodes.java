d9f60c14f1b4e22d471adcfc093379e1536d36e0
Víctor Alan <94298352+TheDracon@users.noreply.github.com>
2023-02-23 10:48:41 +0100

    Made Auth Codes Final

--- src/main/java/org/example/AuthExitCodes.java
+++ src/main/java/org/example/AuthExitCodes.java
@@ -1,17 +1,17 @@
 package org.example;
 
 public class AuthExitCodes {
-    public static int SUCCESS = 0;
-    public static int UNKNOWN_ERROR = -1;
-    public static int TOKEN_EXPIRED_ERROR = 100;
-    public static int TOKEN_NOT_VALID = 101;
-    public static int INCORRECT_PASSWORD = 200;
-    public static int INVALID_PASSWORD = 201;
-    public static int NON_EXISTING_EMAIL = 300;
-    public static int INVALID_EMAIL = 301;
-    public static int EMAIL_TAKEN = 302;
-    public static int INVALID_USERNAME = 400;
-    public static int USERNAME_TAKEN = 400;
+    public static final int SUCCESS = 0;
+    public static final int UNKNOWN_ERROR = -1;
+    public static final int TOKEN_EXPIRED_ERROR = 100;
+    public static final int TOKEN_NOT_VALID = 101;
+    public static final int INCORRECT_PASSWORD = 200;
+    public static final int INVALID_PASSWORD = 201;
+    public static final int NON_EXISTING_EMAIL = 300;
+    public static final int INVALID_EMAIL = 301;
+    public static final int EMAIL_TAKEN = 302;
+    public static final int INVALID_USERNAME = 400;
+    public static final int USERNAME_TAKEN = 400;
 
 
 }
