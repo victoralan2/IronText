87a8aeb2497a6561aaa71a15202c49fdc29e0c67
Víctor Alan <94298352+TheDracon@users.noreply.github.com>
2023-02-26 08:44:45 +0100

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,34 @@
-package org.example;
+package org.irontext;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-
+        BadWordFilter wordFilter = new BadWordFilter();
+        System.out.println("HELLO WORD!");
+        Thread.sleep(10000);
 
         try {
-            Server server = new Server("localhost", 2918);
-            new Thread(() ->{
-                try {
-                    server.startServer();
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-
-            }).start();
-
-
-            Client client2 = new Client("localhost", 2918);
-            client2.startConnection();
+            System.out.println(wordFilter.filter("hello word! BITCH FUCK YOU"));
         } catch (Exception e){
             e.printStackTrace();
         }
+        Thread.sleep(1000);
+//
+//        try {
+//            Server server = new Server("localhost", 2918);
+//            new Thread(() ->{
+//                try {
+//                    server.startServer();
+//                } catch (Exception e) {
+//                    throw new RuntimeException(e);
+//                }
+//
+//            }).start();
+//
+//
+//            Client client2 = new Client("localhost", 2918);
+//            client2.startConnection();
+//        } catch (Exception e){
+//            e.printStackTrace();
+//        }
     }
 }
