5e94a83a4ca780630866fd1e10b59b116a8815f0
TheDracon <vicvarcas2007@gmail.com>
2023-03-02 20:50:01 +0100

    Deleted tests

--- src/main/java/org/irontext/Main.java
+++ src/main/java/org/irontext/Main.java
@@ -2,46 +2,13 @@ package org.irontext;
 
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-
+    public static void main(String[] args) {
         try {
-            Server server = new Server("localhost", 2918);
-            new Thread(() ->{
-                try {
-                    server.startServer();
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-
-            }).start();
-
-
-            Client client = new Client("localhost", 2918);
-            new Thread(()->{
-                try {
-                    client.startConnection(1);
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-            }).start();
-            Client client2 = new Client("localhost", 2918);
-            new Thread(()->{
-                try {
-                    client2.startConnection(2);
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-            }).start();
-            Client client3 = new Client("localhost", 2918);
-            new Thread(()->{
-                try {
-                    client3.startConnection(3);
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-            }).start();
+            Server server = new Server("localhost", 1252);
+            server.startServer();
         } catch (Exception e){
             e.printStackTrace();
         }
+
     }
 }
