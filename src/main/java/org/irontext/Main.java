fe49b140f263404c6646d28c010522358fd8c630
TheDracon_ <vicvarcas2007@gmail.com>
2023-03-04 15:26:47 +0100

    Added server to Main.class

--- src/main/java/org/irontext/Main.java
+++ src/main/java/org/irontext/Main.java
@@ -2,9 +2,10 @@ package org.irontext;
 
 
 public class Main {
+    public static Server server;
     public static void main(String[] args) {
         try {
-            Server server = new Server("localhost", 1252);
+            server = new Server("localhost", 1252);
             server.startServer();
         } catch (Exception e){
             e.printStackTrace();
