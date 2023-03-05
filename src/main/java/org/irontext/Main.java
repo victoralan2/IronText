a7b8d7d43b4d63d5724f4ad536dd982e4bf4350b
TheDracon_ <vicvarcas2007@gmail.com>
2023-03-05 21:21:14 +0100

    Removed useless imports

--- src/main/java/org/irontext/Main.java
+++ src/main/java/org/irontext/Main.java
@@ -1,15 +1,20 @@
 package org.irontext;
 
 
+import org.irontext.encryption.Hasher;
+
+import java.io.*;
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class Main {
     public static Server server;
     public static void main(String[] args) {
         try {
-            server = new Server("localhost", 1252);
+            server = new Server("192.168.1.101", 52216);
             server.startServer();
         } catch (Exception e){
             e.printStackTrace();
         }
-
     }
 }
