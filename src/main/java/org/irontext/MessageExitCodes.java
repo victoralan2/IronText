67d3a75abb8839fb0f01c370e9b8a08ae0429c0e
TheDracon_ <vicvarcas2007@gmail.com>
2023-03-05 21:21:28 +0100

    Added new MessageExitCode

--- src/main/java/org/irontext/MessageExitCodes.java
+++ src/main/java/org/irontext/MessageExitCodes.java
@@ -5,5 +5,7 @@ public class MessageExitCodes {
     public static final int UNKNOWN_ERROR = -1;
     public static final int INAPPROPRIATE_WORD = 100;
     public static final int INVALID_CHARACTERS = 101;
+
+    public static final int MESSAGE_TOO_LONG = 102;
     public static final int TO_MANY_MESSAGES = 200;
 }
