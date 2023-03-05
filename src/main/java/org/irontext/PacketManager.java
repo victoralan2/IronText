1c03073e5af50dab4356a758ff12ac6223db92a2
TheDracon_ <vicvarcas2007@gmail.com>
2023-03-05 21:22:48 +0100

    Added new package (disconnected)

--- src/main/java/org/irontext/PacketManager.java
+++ src/main/java/org/irontext/PacketManager.java
@@ -3,6 +3,7 @@ package org.irontext;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.sql.PreparedStatement;
@@ -19,6 +20,7 @@ public class PacketManager extends Thread{
 
 
 	public PacketManager(ServerSocket server, SQL sqlDB, Socket clientSocket, UUID clientUUID, EventManager eventManager){
+		System.out.println(clientUUID);
 		this.eventManager = eventManager;
 		this.server = server;
 		this.clientSocket = clientSocket;
@@ -28,26 +30,37 @@ public class PacketManager extends Thread{
 
 	@Override
 	public void run(){
-		while (!clientSocket.isClosed() && !clientSocket.isConnected()){
+		while (!clientSocket.isClosed() && clientSocket.isConnected()){
 			try{
+				System.out.println("listening");
 				DataOutputStream output = new DataOutputStream(clientSocket.getOutputStream());
 				DataInputStream input = new DataInputStream(clientSocket.getInputStream());
+				if (!clientSocket.isConnected() || clientSocket.isClosed() || clientSocket.isInputShutdown() || clientSocket.isOutputShutdown()){
+					System.out.println("broke");
+					break;
+				}
 				int requestType = input.readInt();
+				System.out.println(requestType);
 				// -1 = UNSUBSCRIBE | 0 = SEND MESSAGE | 1 = GET LAST x MESSAGES
-
-				if (requestType == -1){
+				if (requestType == 3){
 					Main.server.eventManager.unSubscribe(clientSocket);
+					Server.connections.remove(clientUUID);
+					clientSocket.close();
+					break;
 				} else if (requestType == 0){
 					String message = input.readUTF();
+					if (message.length() > 2048){
+						System.out.println("Message too long");
 
+					}
+
+					System.out.println("received: " + message);
 					BadWordFilter badWordFilter = new BadWordFilter();
 					ArrayList<String> badWords = badWordFilter.filter(message);
 					System.out.println(message);
-
 					if (!badWords.isEmpty()){
 						if (badWords.size() >= message.split(" ").length / 5 - 1){
 							System.out.println("inappropriate word detected: " + Arrays.toString(badWords.toArray()));
-							output.writeInt(MessageExitCodes.INAPPROPRIATE_WORD);
 							return;
 						}
 					}
@@ -73,7 +86,7 @@ public class PacketManager extends Thread{
 						output.writeInt(MessageExitCodes.TO_MANY_MESSAGES);
 					}
 					PreparedStatement getCountPS = sqlDB.prepareStatement("SELECT COUNT(*) FROM messages ORDER BY message_date DESC LIMIT ?;");
-					PreparedStatement getMessagesPS = sqlDB.prepareStatement("SELECT * FROM messages INNER JOIN users ORDER BY message_date DESC LIMIT ?;");
+					PreparedStatement getMessagesPS = sqlDB.prepareStatement("SELECT * FROM messages LEFT JOIN users ON messages.author_uuid = users.uuid ORDER BY message_date DESC LIMIT ?;");
 					getMessagesPS.setInt(1, numbOfMessages);
 					getCountPS.setInt(1, numbOfMessages);
 					ResultSet countOfRows = getCountPS.executeQuery();
@@ -85,20 +98,29 @@ public class PacketManager extends Thread{
 					ResultSet messagesRows = getMessagesPS.executeQuery();
 
 					// Tells the client how many we are going to send
-					output.writeInt(requestType);
-					output.writeInt(rows);
-					System.out.println(rows);
+					System.out.println("REQTYPE: " + requestType);
+					sendTo(clientSocket, requestType, rows);
+					System.out.println("ROWS: " + rows);
 					messagesRows.next();
-
 
 					// starts sending rows
 					for (int i = 1; i <= rows; i++) {
+						System.out.println("CONT: "+messagesRows.getString("message_content"));
 						sendTo(clientSocket, -1, messagesRows.getString("message_content"), messagesRows.getString("username"), messagesRows.getTimestamp("message_date").getTime());
 						messagesRows.next();
 					}
+
 				}
 			} catch (Exception e){
 				e.printStackTrace();
+				try {
+					clientSocket.close();
+				} catch (IOException ex) {
+					e.printStackTrace();
+				}
+				Server.connections.remove(clientUUID);
+				Main.server.eventManager.unSubscribe(clientSocket);
+				return;
 			}
 		}
 	}
